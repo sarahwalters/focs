aa41587cd185240b5e395906837c9cc384a8df1c
Sarah Walters <swalters4925@gmail.com>
2016-04-26 16:45:47 -0400

    Finishing HW10 test suite

--- homeworks/hw10/homework10.ml
+++ homeworks/hw10/homework10.ml
@@ -1,4 +1,4 @@
-(* 
+(*
 
 HOMEWORK 10
 
@@ -19,7 +19,7 @@ Remarks, if any: Took ~3hr, mostly on AVL insert
  * own implementation.
  *
  * Always make sure you can #use this file before submitting it.
- * Do that in a _fresh_ OCaml shell 
+ * Do that in a _fresh_ OCaml shell
  * It has to load without any errors.
  *
  *)
@@ -40,7 +40,7 @@ let sample = Node(10,Node(3,Node(7,Empty,Empty),
 (* Printing an integer binary tree *)
 
 let pbt bt =
-  let rec loop bt depth = 
+  let rec loop bt depth =
     match bt with
     | Empty -> ()
     | Node(n,left,right) ->
@@ -52,10 +52,10 @@ let pbt bt =
 
 (* Q1 *)
 
-let rec size t = 
+let rec size t =
   match t with
   | Empty -> 0
-  | Node(n,left,right) -> 
+  | Node(n,left,right) ->
       1 + size left + size right
 
 
@@ -69,7 +69,7 @@ let rec sum t =
 let rec height t =
   match t with
   | Empty -> 0
-  | Node(n,left,right) -> 
+  | Node(n,left,right) ->
       1 + max (height left) (height right)
 
 
@@ -77,7 +77,7 @@ let rec fringe t =
   match t with
   | Empty -> []
   | Node(n,left,right) ->
-      if (left = Empty && right = Empty) then [n] 
+      if (left = Empty && right = Empty) then [n]
       else (fringe left) @ (fringe right)
 
 
@@ -123,6 +123,22 @@ let rec bst_insert t x =
           Node(n, left, new_right)
 
 
+(* helper function, used by test suite *)
+let rec is_bst t =
+  match t with
+  | Empty -> true
+  | Node(n,left,right) ->
+      let left_check =
+        match left with
+        | Empty -> true
+        | Node(l,_,_) -> (l <= n) && (is_bst left)
+      in let right_check =
+        match right with
+        | Empty -> true
+        | Node(r,_,_) -> (n < r) && (is_bst right)
+      in left_check && right_check
+
+
 let rec bst_lookup t x =
   match t with
   | Empty -> false
@@ -131,9 +147,10 @@ let rec bst_lookup t x =
       else if (x > n) then bst_lookup right x
       else true
 
+
 let bstify t = (* this is naive *)
   let rec bstify_arr arr =
-    match arr with 
+    match arr with
     | [] -> Empty
     | x::[] -> Node(x, Empty, Empty)
     | x::xs -> bst_insert (bstify_arr xs) x
@@ -141,43 +158,43 @@ let bstify t = (* this is naive *)
 
 
 (* AVL tree rotate diagrams from http://www.geeksforgeeks.org/avl-tree-set-1-insertion/ *)
-(* 
-         z                                      y 
+(*
+         z                                      y
         / \                                   /   \
        y   T4      Right Rotate              x      z
-      / \          - - - - - - - - ->      /  \    /  \ 
+      / \          - - - - - - - - ->      /  \    /  \
      x   T3                               T1  T2  T3  T4
     / \
   T1   T2
 *)
-let avl_leftleft z sub_y t4 = 
+let avl_leftleft z sub_y t4 =
   match sub_y with (* sub_y is the subtree rooted at y *)
   | Empty -> failwith "Left-left: first left rotate failed"
   | Node(y,sub_x,t3) ->
       match sub_x with (* sub_x is the subtree rooted at x *)
       | Empty -> failwith "Left-left: second left rotate failed"
-      | Node(x,t1,t2) -> Node(y, Node(x,t1,t2), Node(z,t3,t4))    
+      | Node(x,t1,t2) -> Node(y, Node(x,t1,t2), Node(z,t3,t4))
 
-(* 
+(*
      z                              x
-    / \                            /  \ 
+    / \                            /  \
    y   T4  Left Right Rotate     y      z
   / \      - - - - - - - - ->   / \    / \
 T1   x                        T1  T2 T3  T4
-    / \                       
-  T2   T3                     
+    / \
+  T2   T3
 *)
 let avl_leftright z sub_y t4 =
   match sub_y with
   | Empty -> failwith "Left-right: left rotate failed"
-  | Node(y,t1,sub_x) -> 
+  | Node(y,t1,sub_x) ->
       match sub_x with
       | Empty -> failwith "Left-right: right rotate failed"
       | Node(x,t2,t3) -> Node(x, Node(y,t1,t2), Node(z,t3,t4))
 
-(* 
+(*
   z                                y
- /  \                            /   \ 
+ /  \                            /   \
 T1   y     Left Rotate          z      x
     /  \   - - - - - - - ->    / \    / \
    T2   x                     T1  T2 T3  T4
@@ -187,26 +204,26 @@ T1   y     Left Rotate          z      x
 let avl_rightright z t1 sub_y =
   match sub_y with
   | Empty -> failwith "Right-right: first right rotate failed"
-  | Node(y,t2,sub_x) -> 
+  | Node(y,t2,sub_x) ->
       match sub_x with
       | Empty -> failwith "Right-right: second right rotate failed"
       | Node(x,t3,t4) -> Node(y, Node(z,t1,t2), Node(x,t3,t4))
 
-(* 
+(*
    z                               x
-  / \                             /  \ 
+  / \                             /  \
 T1   y   Right Left Rotate      z      y
     / \  - - - - - - - - ->    / \    / \
    x   T4                    T1  T2  T3  T4
-  / \                          
-T2   T3                         
+  / \
+T2   T3
 *)
-let avl_rightleft z t1 sub_y = 
+let avl_rightleft z t1 sub_y =
   match sub_y with
   | Empty -> failwith "Right-left: right rotate failed"
-  | Node(y,sub_x,t4) -> 
+  | Node(y,sub_x,t4) ->
       match sub_x with
-      | Empty -> failwith "Right-left: left rotate failed" 
+      | Empty -> failwith "Right-left: left rotate failed"
       | Node(x,t2,t3) -> Node(x, Node(z,t1,t2), Node(y,t3,t4))
 
 let rec avl_insert t x =
@@ -214,7 +231,7 @@ let rec avl_insert t x =
   | Empty -> Node(x, Empty, Empty)
   | Node(n,left,right) ->
       if (x <= n) then (* inserting left -- left left & right left rotate cases *)
-        let new_left = avl_insert left x in 
+        let new_left = avl_insert left x in
           let lh = height new_left in
           let rh = height right in
             if (lh - rh > 1) then avl_leftleft n new_left right
--- homeworks/hw10/homework10_test.ml
+++ homeworks/hw10/homework10_test.ml
@@ -1,6 +1,10 @@
 open OUnit2;;
 open Homework10;;
 
+(* TEST SETUP *)
+let sample1 = (Node(1,Empty,Empty));;
+let sample2 = (Node(1,Node(2,Empty,Empty),Node(3,Empty,Empty)));;
+
 let mapFn = fun x -> x * x;;
 let unmapped1 = Node(2,Empty,Empty);;
 let mapped1 = Node(4,Empty,Empty);;
@@ -16,12 +20,28 @@ let mappedSample = Node(100,
                         Node(4356,Empty,Empty)),
                       Empty));;
 
-let sample1 = (Node(1,Empty,Empty));;
-let sample2 = (Node(1,Node(2,Empty,Empty),Node(3,Empty,Empty)));;
+let insert1 = (Node (5,Empty,Empty));;
+let insert2 = (Node (5,Node(2,Empty,Empty),Node(8,Empty,Empty)));;
 
 let lookup1 = (Node (5,Empty,Empty));;
 let lookup2 = (Node (5,Node(2,Empty,Empty),Node(8,Empty,Empty)));;
 
+let avl1 = avl_insert Empty 1;;
+let avl2 = avl_insert avl1 2;;
+let avl3 = avl_insert avl2 3;;
+let avl4 = avl_insert avl3 4;;
+let avl5 = avl_insert avl4 5;;
+let avl6 = avl_insert avl5 6;;
+
+let bst1 = bst_insert Empty 10;;
+let bst2 = bst_insert insert1 2;;
+let bst3 = bst_insert insert1 8;;
+let bst4 = bst_insert insert2 10;;
+let bst5 = bst_insert insert2 4;;
+let bst6 = bst_insert insert2 1;;
+let bst7 = bst_insert insert2 6;;
+
+(* TESTS *)
 let size_test test_ctxt =
   assert_equal 0 (size Empty);;
   assert_equal 1 (size sample1);;
@@ -74,8 +94,34 @@ let inorder_test test_ctxt =
   assert_equal [2; 1; 3] (inorder sample2);;
   assert_equal [7; 3; 5; 10; 99; 66; 6] (inorder sample);;
 
-let bst_insert_test test_ctxt = 
-  assert_equal [] (inorder Empty);; (* TODO *)
+let bst_insert_test test_ctxt =
+  assert_equal 1 (size bst1);;
+  assert_equal 1 (height bst1);;
+  assert_equal true (is_bst bst1);;
+
+  assert_equal 2 (size bst2);;
+  assert_equal 2 (height bst2);;
+  assert_equal true (is_bst bst2);;
+
+  assert_equal 2 (size bst3);;
+  assert_equal 2 (height bst3);;
+  assert_equal true (is_bst bst3);;
+
+  assert_equal 4 (size bst4);;
+  assert_equal 3 (height bst4);;
+  assert_equal true (is_bst bst4);;
+
+  assert_equal 4 (size bst5);;
+  assert_equal 3 (height bst5);;
+  assert_equal true (is_bst bst5);;
+
+  assert_equal 4 (size bst6);;
+  assert_equal 3 (height bst6);;
+  assert_equal true (is_bst bst6);;
+
+  assert_equal 4 (size bst7);;
+  assert_equal 3 (height bst7);;
+  assert_equal true (is_bst bst7);;
 
 let bst_lookup_test test_ctxt =
   assert_equal false (bst_lookup Empty 1);;
@@ -85,11 +131,36 @@ let bst_lookup_test test_ctxt =
   assert_equal true (bst_lookup lookup2 2);;
   assert_equal true (bst_lookup lookup2 8);;
 
-let bstify_test test_ctxt = 
-  assert_equal [] (inorder Empty);; (* TODO *)
+let bstify_test test_ctxt =
+  assert_equal true (is_bst (bstify Empty));;
+  assert_equal true (is_bst (bstify (Node(1,Empty,Empty))));;
+  assert_equal true (is_bst (bstify (Node(4,Node(7,Empty,Empty),Node(5,Empty,Empty)))));;
+  assert_equal true (is_bst (bstify sample));;
 
-let avl_insert_test test_ctxt = 
-  assert_equal [] (inorder Empty);; (* TODO *)
+let avl_insert_test test_ctxt =
+  assert_equal true (is_bst avl1);;
+  assert_equal 1 (size avl1);;
+  assert_equal 1 (height avl1);;
+
+  assert_equal true (is_bst avl2);;
+  assert_equal 2 (size avl2);;
+  assert_equal 2 (height avl2);;
+
+  assert_equal true (is_bst avl3);;
+  assert_equal 3 (size avl3);;
+  assert_equal 2 (height avl3);;
+
+  assert_equal true (is_bst avl4);;
+  assert_equal 4 (size avl4);;
+  assert_equal 3 (height avl4);;
+
+  assert_equal true (is_bst avl5);;
+  assert_equal 5 (size avl5);;
+  assert_equal 3 (height avl5);;
+
+  assert_equal true (is_bst avl6);;
+  assert_equal 6 (size avl6);;
+  assert_equal 3 (height avl6);;
 
 let suite =
   "suite">:::
@@ -108,4 +179,4 @@ let suite =
      "avl_insert_test">::avl_insert_test]
 
 let () =
-   run_test_tt_main suite
+  run_test_tt_main suite
